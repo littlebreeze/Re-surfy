2bf8d90887ec09ef50075485b8e398f3306e6b1b
kibeom <kibeom2000@naver.com>
2023-04-07 13:38:22 +0900

    test_push

--- resurfy/src/main/java/org/zerock/mapper/RecipeMapper.java
+++ resurfy/src/main/java/org/zerock/mapper/RecipeMapper.java
@@ -1,5 +1,26 @@
 package org.zerock.mapper;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+import org.zerock.domain.Criteria;
+import org.zerock.domain.IngredientVO;
+import org.zerock.domain.RecipeVO;
+import org.zerock.domain.StepVO;
+
 public interface RecipeMapper {
 	public RecipeVO read(Long bno);
+	public StepVO read(Long bno);
+	public IngredientVO read(long bno);
+	public void insert(RecipeVO board);
+	public void insert(StepVO board);
+	public void insert(IngredientVO board);
+	public void update(RecipeVO board);
+	public void update(StepVO board);
+	public void update(IngredientVO board);
+	public Long delete(Long bno);
+	public List<RecipeVO> getList();
+	public  List<RecipeVO> getListWithPaging(Criteria cri);
+	public Long getTotalCount(Criteria cri);
+	public void updateReplyCnt(@Param("bno") Long bno, @Param("amount") Long amount)
 }
