96d6fa721cdd7c5c159e12a84dc6b3e6108ec86c
yoonj01 <jinjoo9085@naver.com>
2023-04-12 13:25:30 +0900

    RecipeMapper.xml, tests 수정&추가

--- resurfy/src/main/java/org/zerock/mapper/RecipeMapper.java
+++ resurfy/src/main/java/org/zerock/mapper/RecipeMapper.java
@@ -3,6 +3,7 @@ package org.zerock.mapper;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 import org.zerock.domain.Criteria;
 import org.zerock.domain.IngredientVO;
 import org.zerock.domain.RecipeVO;
@@ -10,17 +11,23 @@ import org.zerock.domain.StepVO;
 
 public interface RecipeMapper {
 	public RecipeVO readRecipe(Long bno);
-	public StepVO readStep(Long sno);
-	public IngredientVO readIngredient(long ino);
+	public List<StepVO> readStep(Long bno);
+	public List<IngredientVO> readIngredient(Long bno);
 	public void insertRecipe(RecipeVO board);
 	public void insertStep(StepVO board);
 	public void insertIngredient(IngredientVO board);
-	public void updateRecipe(RecipeVO board);
-	public void updateStep(StepVO board);
-	public void updateIngredient(IngredientVO board);
-	public Long delete(Long bno);
-	public List<RecipeVO> getList();
-	public  List<RecipeVO> getListWithPaging(Criteria cri);
-	public int getTotalCount(Criteria cri);
+	public int updateRecipe(RecipeVO board);
+	public int updateStep(StepVO board);
+	public int updateIngredient(IngredientVO board);
+	
+	public int deleteRecipe(Long bno);
+	public int deleteStep(Long bno);
+	public int deleteIngredient(Long bno);
+	
+	public List<RecipeVO> getRecipeList();
+	public List<StepVO> getStepList();
+	public List<IngredientVO> getIngreList();
+	public List<RecipeVO> getListWithPaging(Criteria cri);
+	public Long getTotalCount(Criteria cri);
 	public void updateReplyCnt(@Param("bno") Long bno, @Param("amount") Long amount);
 }
