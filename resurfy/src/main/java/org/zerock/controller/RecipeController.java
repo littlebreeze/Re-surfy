b34807f2898faec0920702581a68e98a5999103f
kibeom <kibeom2000@naver.com>
2023-04-22 14:25:34 +0900

    commit_0421

--- resurfy/src/main/java/org/zerock/controller/RecipeController.java
+++ resurfy/src/main/java/org/zerock/controller/RecipeController.java
@@ -41,7 +41,6 @@ public class RecipeController {
 	public void register() {
 		log.info("/registerRecipe");
 	}
-<<<<<<< HEAD
 	/*
 	 * @PostMapping("/registerRecipe") public String register(RecipeVO recipe,
 	 * StepVO step, IngredientVO ingre, RedirectAttributes rttr) {
@@ -52,11 +51,7 @@ public class RecipeController {
 	 * 
 	 * }
 	 */
-	@GetMapping({"/detail", "/modify"})
-=======
-
 	@GetMapping({ "/detail", "/modify" })
->>>>>>> branch 'main' of https://github.com/barcataeeon/Re-surfy.git
 	public void get(HttpServletRequest request, @RequestParam("bno") Long bno, Model model) {
 		log.info("/detail or modify");
 		model.addAttribute("recipe", rService.get(bno));
@@ -130,26 +125,16 @@ public class RecipeController {
 	}
 
 	@GetMapping("/TopTen")
-<<<<<<< HEAD
 	public void ListSort(Model model ,@ModelAttribute("cri") Criteria cri) {
 		model.addAttribute("sortByReply",rService.sortByReplyCnt());
 		model.addAttribute("sortByVisit",rService.sortByVisitCnt());
-=======
-	public void ListSort(Model model) {
-		model.addAttribute("sortByReply", rService.sortByReplyCnt());
-		model.addAttribute("sortByVisit", rService.sortByVisitCnt());
->>>>>>> branch 'main' of https://github.com/barcataeeon/Re-surfy.git
+
 	}
-<<<<<<< HEAD
+
 	
 	
 	public void get(@RequestParam("bno") Long bno, @ModelAttribute("cri") Criteria cri , Model model) {
-		
-=======
-
-	public void get(@RequestParam("bno") Long bno, @ModelAttribute("cri") Criteria cri, Model model) {
-
->>>>>>> branch 'main' of https://github.com/barcataeeon/Re-surfy.git
+	
 	}
 
 	public String modify(@RequestParam("bno") Long bno, @ModelAttribute("cri") Criteria cri, RedirectAttributes rttr) {
